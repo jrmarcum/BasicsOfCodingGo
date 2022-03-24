b815475e08943f6dcfbf9a8a9308bab05ba63387
jrmarcum <81428316+jrmarcum@users.noreply.github.com>
2022-03-24 17:39:37 -0400

    Add files via upload

---
+++ 41_sorting-by-functions/sorting-by-functions.go
@@ -0,0 +1,52 @@
+// This work was originally from Mark McGranaghan at (https://github.com/mmcgrana/gobyexample).
+// And licensed under a Creative Commons Attribution 3.0 Unported License (http://creativecommons.org/licenses/by/3.0/)
+// It has been used to provide an example base for multiple languages to provide a basis of comparitive programming
+// language study on sytax and simplicity of the languages as far as number of lines of code and operations required
+// to perform the same task, as well as compile and run speed combined.
+
+
+// Sometimes we'll want to sort a collection by something
+// other than its natural order. For example, suppose we
+// wanted to sort strings by their length instead of
+// alphabetically. Here's an example of custom sorts
+// in Go.
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// In order to sort by a custom function in Go, we need a
+// corresponding type. Here we've created a `byLength`
+// type that is just an alias for the builtin `[]string`
+// type.
+type byLength []string
+
+// We implement `sort.Interface` - `Len`, `Less`, and
+// `Swap` - on our type so we can use the `sort` package's
+// generic `Sort` function. `Len` and `Swap`
+// will usually be similar across types and `Less` will
+// hold the actual custom sorting logic. In our case we
+// want to sort in order of increasing string length, so
+// we use `len(s[i])` and `len(s[j])` here.
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+// With all of this in place, we can now implement our
+// custom sort by converting the original `fruits` slice
+// to `byLength`, and then use `sort.Sort` on that typed
+// slice.
+func main() {
+	fruits := []string{"peach", "banana", "kiwi"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
---
+++ 42_panic/panic.go
@@ -0,0 +1,32 @@
+// This work was originally from Mark McGranaghan at (https://github.com/mmcgrana/gobyexample).
+// And licensed under a Creative Commons Attribution 3.0 Unported License (http://creativecommons.org/licenses/by/3.0/)
+// It has been used to provide an example base for multiple languages to provide a basis of comparitive programming
+// language study on sytax and simplicity of the languages as far as number of lines of code and operations required
+// to perform the same task, as well as compile and run speed combined.
+
+
+// A `panic` typically means something went unexpectedly
+// wrong. Mostly we use it to fail fast on errors that
+// shouldn't occur during normal operation, or that we
+// aren't prepared to handle gracefully.
+
+package main
+
+import "os"
+
+func main() {
+
+	// We'll use panic throughout this site to check for
+	// unexpected errors. This is the only program on the
+	// site designed to panic.
+	panic("a problem")
+
+	// A common use of panic is to abort if a function
+	// returns an error value that we don't know how to
+	// (or want to) handle. Here's an example of
+	// `panic`king if we get an unexpected error when creating a new file.
+	_, err := os.Create("/tmp/file")
+	if err != nil {
+		panic(err)
+	}
+}
