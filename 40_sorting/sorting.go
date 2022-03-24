2ea000e5933996868ab7cbeb7f56c1d6822ca468
jrmarcum <81428316+jrmarcum@users.noreply.github.com>
2022-03-24 17:38:51 -0400

    Add files via upload

---
+++ 40_sorting/sorting.go
@@ -0,0 +1,38 @@
+// This work was originally from Mark McGranaghan at (https://github.com/mmcgrana/gobyexample).
+// And licensed under a Creative Commons Attribution 3.0 Unported License (http://creativecommons.org/licenses/by/3.0/)
+// It has been used to provide an example base for multiple languages to provide a basis of comparitive programming
+// language study on sytax and simplicity of the languages as far as number of lines of code and operations required
+// to perform the same task, as well as compile and run speed combined.
+
+
+// Go's `sort` package implements sorting for builtins
+// and user-defined types. We'll look at sorting for
+// builtins first.
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	// Sort methods are specific to the builtin type;
+	// here's an example for strings. Note that sorting is
+	// in-place, so it changes the given slice and doesn't
+	// return a new one.
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	// An example of sorting `int`s.
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
+
+	// We can also use `sort` to check if a slice is
+	// already in sorted order.
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
+}
